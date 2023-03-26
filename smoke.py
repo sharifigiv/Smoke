da09c14444fa310ee925b52fe1e735cbb5dc8734
Miumen <sharifigiv5954@gmail.com>
2023-03-26 23:05:52 +0330

    Add update func

--- smoke.py
+++ smoke.py
@@ -4,4 +4,10 @@ class Smoke:
         self.y = y
         self.opacity = 200
 
-    
+        self.vel = [0, 0]
+
+    def update(self):
+        self.x += self.vel[0]
+        self.y += self.vel[1]
+
+        self.opacity -= 0.2
