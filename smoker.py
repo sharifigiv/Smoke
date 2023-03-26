3d1dff6f247a8fa8a358953df26988c9bf49c081
Miumen <sharifigiv5954@gmail.com>
2023-03-26 23:39:21 +0330

    Fix update

--- smoker.py
+++ smoker.py
@@ -8,16 +8,17 @@ class Smoker:
         self.smoking = True
         self.smokes = []
 
-    def create_smoke(self, direction: list):
-        smk = Smoke(self.x, self.y)
+    def create_smoke(self, direction: list, change = [0, 0]):
+        smk = Smoke(self.x + change[0], self.y + change[1])
         smk.vel = direction
 
         self.smokes.append(smk)
 
     def update(self):
         if self.smoking:
-            self.create_smoke()
+            for s in self.smokes:
+                s.update()
 
-        for s in self.smokes:
-            if s.opacity <= 0:
-                self.smokes.remove(s)
+                if s.opacity <= 0:
+                    self.smokes.remove(s)
+                    
